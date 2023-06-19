0ab3a3caa2aec269f214c616c58e150ce0454584
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-06-19 11:49:38 +0700

    Membuat prosedur try array

--- exceptionStandar(1)/exceptionStandar(1).cpp
+++ exceptionStandar(1)/exceptionStandar(1).cpp
@@ -6,7 +6,13 @@
 using namespace std;
 
 int main(){
-
+	cout << "Awal program" << endl; // penanda 1:...
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array integer 3 elemen
+		//cout<<data.at(5)<<endl;
+		//Memanggil array elemen ke 5
+	}
 
 }
 
