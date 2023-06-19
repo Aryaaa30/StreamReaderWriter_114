d50a1a8624bcc25ebb2370d6894172cb922359b1
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-06-19 11:42:44 +0700

    Membuat ofstream dengan nama file outfile dan setelah selesai di close

--- ioFileWithPath(1)/ioFileWithPath(1).cpp
+++ ioFileWithPath(1)/ioFileWithPath(1).cpp
@@ -10,6 +10,26 @@ int main(){
 	cout << "Masukkan nama file : ";
 	cin >> Namafile;
 
+	// Membuka file dalam mode menulis.
+	ofstream outfile;
+	// Menunjuk ke sebuah nama file
+	outfile.open(Namafile, ios::out);
+
+	cout << ">= Menulis file, '\q' untuk keluar" << endl;
+
+	//unlimited loop unutk menulis
+	while (true) {
+		cout << "-";
+		//Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		// Menukis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarang tutup filenya 
+	outfile.close();
+
 }
 
 
